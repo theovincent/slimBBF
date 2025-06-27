e12a604e87df76cec28e46fa412c033304f57bd4
yogesh1q2w <yogesh120699@gmail.com>
2025-06-27 12:10:42 +0200

    sum tree updated for BBF RB

--- tests/test_sum_tree.py
+++ tests/test_sum_tree.py
@@ -11,54 +11,50 @@ class SumTreeTest(parameterized.TestCase):
 
     def setUp(self):
         super(SumTreeTest, self).setUp()
-        self._tree = sum_tree.SumTree(capacity=100)
+        self._tree = sum_tree.DeterministicSumTree(capacity=100)
 
     def test_negative_capacity_raises(self):
         with self.assertRaises(AssertionError):
-            sum_tree.SumTree(capacity=-1)
+            sum_tree.DeterministicSumTree(capacity=-1)
 
     def test_negative_value_raises(self):
         with self.assertRaises(AssertionError):
             self._tree.set(0, -1)
 
     def test_set_small_capacity(self):
-        tree = sum_tree.SumTree(capacity=1)
+        tree = sum_tree.DeterministicSumTree(capacity=1)
         tree.set(0, 1.5)
-        self.assertEqual(tree.root, 1.5)
+        self.assertEqual(tree._total_priority(), 1.5)
 
     def test_set_and_get_value(self):
         self._tree.set(0, 1.0)
         self.assertEqual(self._tree.get(0), 1.0)
 
         # Validate that all nodes on the leftmost branch have value 1.
-        leaf_index = self._tree._first_leaf_offset
+        leaf_index = self._tree.low_idx
         while leaf_index > 0:
             leaf_index = leaf_index // 2
-            self.assertEqual(self._tree._nodes[leaf_index], 1.0)
+            self.assertEqual(self._tree.nodes[leaf_index], 1.0)
 
-    def test_set_and_get_values_vectorized(self):
-        self._tree.set(
-            np.array([1, 2], dtype=np.int32),
-            np.array([3.0, 4.0], dtype=np.float32),
-        )
+    def test_set_and_get_values(self):
+        self._tree.set(1, 3.0)
+        self._tree.set(2, 4.0)
         self.assertEqual(self._tree.get(1), 3.0)
         self.assertEqual(self._tree.get(2), 4.0)
-        self.assertEqual(self._tree.root, 7.0)
+        self.assertEqual(self._tree._total_priority(), 7.0)
 
     def test_set_with_duplicates(self):
-        self._tree.set(
-            np.array([1, 1, 1, 2, 2], dtype=np.int32),
-            np.array([3.0, 3.0, 3.0, 4.0, 4.0], dtype=np.float32),
-        )
+        for index, priority in zip([1, 1, 1, 2, 2], [3.0, 3.0, 3.0, 4.0, 4.0]):
+            self._tree.set(index, priority)
         self.assertEqual(self._tree.get(1), 3.0)
         self.assertEqual(self._tree.get(2), 4.0)
-        self.assertEqual(self._tree.root, 7.0)
+        self.assertEqual(self._tree._total_priority(), 7.0)
 
     def test_capacity_greater_than_requested(self):
-        self.assertGreaterEqual(self._tree._nodes.size, 100)
+        self.assertGreaterEqual(self._tree.nodes.size, 100)
 
     def test_query_empty_tree(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(AssertionError):
             self._tree.query(1.0)
 
     def test_query_value(self):
@@ -73,16 +69,16 @@ class SumTreeTest(parameterized.TestCase):
            [1.5]  [1.0]
         [0.5 1.0 0.5 0.5]
         """
-        tree = sum_tree.SumTree(capacity=4)
-        tree.set(
-            np.array([0, 1, 2, 3], dtype=np.int32),
-            np.array([0.5, 1.0, 0.5, 0.5], dtype=np.float32),
-        )
-        self.assertEqual(tree.root, 2.5)
-        self.assertEqual(tree._depth, 3)
-        self.assertEqual(tree._nodes.size, 7)
+        tree = sum_tree.DeterministicSumTree(capacity=4)
+        for index, priority in zip(
+            np.array([0, 1, 2, 3], dtype=np.int32), np.array([0.5, 1.0, 0.5, 0.5], dtype=np.float32)
+        ):
+            tree.set(index, priority)
+        self.assertEqual(tree._total_priority(), 2.5)
+        self.assertEqual(tree.depth, 2)
+        self.assertEqual(tree.nodes.size, 7)
         np.testing.assert_array_equal(
-            tree.query(np.array([1.5, 1.0])),
+            [tree.query(i) for i in np.array([0.6, 0.4])],
             np.array([2, 1], np.int32),
         )
 
@@ -94,16 +90,16 @@ class SumTreeTest(parameterized.TestCase):
            [1.5]  [1.0]
         [0.5 1.0 0.5 0.5]
         """
-        tree = sum_tree.SumTree(capacity=4)
-        tree.set(
-            np.array([0, 1, 2, 3], dtype=np.int32),
-            np.array([0.5, 1.0, 0.5, 0.5], dtype=np.float32),
-        )
+        tree = sum_tree.DeterministicSumTree(capacity=4)
+        for index, priority in zip(
+            np.array([0, 1, 2, 3], dtype=np.int32), np.array([0.5, 1.0, 0.5, 0.5], dtype=np.float32)
+        ):
+            tree.set(index, priority)
         tree.set(0, 0.25)
-        self.assertEqual(tree.root, 2.25)
-        self.assertEqual(tree.query(0.249), 0)
-        self.assertEqual(tree.query(0.5), 1)
-        self.assertEqual(tree.query(1.25), 2)
+        self.assertEqual(tree._total_priority(), 2.25)
+        self.assertEqual(tree.query(0.111), 0)
+        self.assertEqual(tree.query(0.222), 1)
+        self.assertEqual(tree.query(0.556), 2)
 
     def test_query_values_vectorized_large_tree(self):
         #
@@ -114,16 +110,14 @@ class SumTreeTest(parameterized.TestCase):
           [2]   [2]   [2]   [2]
         [1, 1, 1, 1, 1, 1, 1, 1]
         """
-        tree = sum_tree.SumTree(capacity=8)
-        tree.set(
-            np.arange(8, dtype=np.int32),
-            np.ones((8,), dtype=np.float32),
-        )
-        self.assertEqual(tree.root, 8.0)
-        self.assertEqual(tree._depth, 4)
-        self.assertEqual(tree._nodes.size, 15)
+        tree = sum_tree.DeterministicSumTree(capacity=8)
+        for index, priority in zip(np.arange(8, dtype=np.int32), np.ones((8,), dtype=np.float32)):
+            tree.set(index, priority)
+        self.assertEqual(tree._total_priority(), 8.0)
+        self.assertEqual(tree.depth, 3)
+        self.assertEqual(tree.nodes.size, 15)
         np.testing.assert_array_equal(
-            tree.query(np.arange(8, dtype=np.int32)),
+            [tree.query(i) for i in np.linspace(start=0, stop=1, num=8)],
             np.arange(8, dtype=np.int32),
         )
 
