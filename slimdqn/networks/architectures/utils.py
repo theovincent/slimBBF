5599c79bf80c13ebd35007d1ea5972011078e423
yogesh1q2w <yogesh120699@gmail.com>
2025-07-10 16:46:48 +0200

    augmentation tools

---
+++ slimdqn/networks/architectures/utils.py
@@ -0,0 +1,57 @@
+import functools
+
+import jax
+import jax.numpy as jnp
+
+
+@functools.partial(jax.vmap, in_axes=(0, 0, 0, None))
+def _crop_with_indices(img, x, y, cropped_shape):
+    cropped_image = jax.lax.dynamic_slice(img, [x, y, 0], cropped_shape[1:])
+    return cropped_image
+
+
+def _per_image_random_crop(key, img, cropped_shape):
+    """Random crop an image."""
+    batch_size, width, height = cropped_shape[:-1]
+    key_x, key_y = jax.random.split(key, 2)
+    x = jax.random.randint(key_x, shape=(batch_size,), minval=0, maxval=img.shape[1] - width)
+    y = jax.random.randint(key_y, shape=(batch_size,), minval=0, maxval=img.shape[2] - height)
+    return _crop_with_indices(img, x, y, cropped_shape)
+
+
+def _intensity_aug(key, x, scale=0.05):
+    """Follows the code in Schwarzer et al. (2020) for intensity augmentation."""
+    r = jax.random.normal(key, shape=(x.shape[0], 1, 1, 1))
+    noise = 1.0 + (scale * jnp.clip(r, -2.0, 2.0))
+    return x * noise
+
+
+@functools.partial(jax.jit)
+def drq_augment(key, obs, img_pad=4):
+    """Padding and cropping for DrQ."""
+    flat_obs = obs.reshape(-1, *obs.shape[-3:])
+    paddings = [(0, 0), (img_pad, img_pad), (img_pad, img_pad), (0, 0)]
+    cropped_shape = flat_obs.shape
+    # The reference uses ReplicationPad2d in pytorch, but it is not available in Jax. Use 'edge' instead.
+    flat_obs = jnp.pad(flat_obs, paddings, "edge")
+    key1, key2 = jax.random.split(key, num=2)
+    cropped_obs = _per_image_random_crop(key2, flat_obs, cropped_shape)
+    aug_obs = _intensity_aug(key1, cropped_obs)
+    return aug_obs.reshape(*obs.shape)
+
+
+def normalize_and_augment(x, rng):
+    """Input normalization and if specified, data augmentation."""
+    out = x.astype(jnp.float32) / 255.0
+    out = drq_augment(rng, out)
+    return out
+
+
+def renormalize(tensor, has_batch=False):
+    shape = tensor.shape
+    if not has_batch:
+        tensor = jnp.expand_dims(tensor, 0)
+    tensor = tensor.reshape(tensor.shape[0], -1)
+    max_value = jnp.max(tensor, axis=-1, keepdims=True)
+    min_value = jnp.min(tensor, axis=-1, keepdims=True)
+    return ((tensor - min_value) / (max_value - min_value + 1e-5)).reshape(*shape)
