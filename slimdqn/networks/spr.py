e4080dc346f7c7fc37928e7d09b637ef11eaab45
yogesh1q2w <yogesh120699@gmail.com>
2025-07-10 17:23:58 +0200

    WIP: spr

---
+++ slimdqn/networks/spr.py
@@ -0,0 +1,141 @@
+from functools import partial
+
+import jax
+import jax.numpy as jnp
+import optax
+from flax.core import FrozenDict
+
+from slimdqn.networks.architectures.dqn import SPRNet
+from slimdqn.sample_collection.subseq_replay_buffer import SubsequenceReplayBuffer, SubsequenceReplayElement
+
+
+class SPR:
+    def __init__(
+        self,
+        key: jax.random.PRNGKey,
+        observation_dim,
+        n_actions,
+        n_bins: int,
+        features: list,
+        learning_rate: float,
+        gamma: float,
+        update_horizon: int,
+        update_to_data: int,
+        target_update_frequency: int,
+        min_value: float,
+        max_value: float,
+        adam_eps: float = 1e-8,
+    ):
+        self.n_bins = n_bins
+        self.network = SPRNet(features, n_actions * self.n_bins)
+
+        def network_apply_fn_spr(params, states, actions):
+            q_logits, latent_predictions, latent_targets = self.network.apply(params, states, actions)
+            q_logits = q_logits.reshape((n_actions, self.n_bins))
+            return q_logits, latent_predictions, latent_targets
+
+        self.network.apply_fn = network_apply_fn_spr
+        self.apply_fn_inference = lambda params, state: self.network.apply(params, state).reshape(
+            (n_actions, self.n_bins)
+        )
+
+        self.params = self.network.init(key, jnp.zeros(observation_dim, dtype=jnp.float32))
+
+        self.optimizer = optax.adam(learning_rate, eps=adam_eps)
+        self.optimizer_state = self.optimizer.init(self.params)
+
+        self.gamma = gamma
+        self.update_horizon = update_horizon
+        self.update_to_data = update_to_data
+        assert target_update_frequency == 1, "SPR has TUF=1"
+        self.target_update_frequency = target_update_frequency
+        self.cumulated_loss = 0
+        self.cumulated_unsupported_prob = 0
+        self.support = jnp.linspace(min_value, max_value, self.n_bins, dtype=jnp.float32)
+
+    def update_online_params(self, step: int, replay_buffer: SubsequenceReplayBuffer):
+        if step % self.update_to_data == 0:
+            batch_samples, metadata = replay_buffer.sample()
+
+            self.params, self.optimizer_state, losses, unsupported_prob = self.learn_on_batch(
+                self.params, self.optimizer_state, batch_samples, metadata["probabilities"]
+            )
+            metadata.update({"loss": losses})
+            replay_buffer.update(metadata)
+
+            self.cumulated_loss += losses.mean()
+            self.cumulated_unsupported_prob += unsupported_prob
+
+    def update_target_params(self, step: int):
+        if step % self.target_update_frequency == 0:
+
+            logs = {
+                "loss": self.cumulated_loss / (self.target_update_frequency / self.update_to_data),
+                "unsupported_prob": self.cumulated_unsupported_prob
+                / (self.target_update_frequency / self.update_to_data),
+            }
+            self.cumulated_loss = 0
+            self.cumulated_unsupported_prob = 0
+
+            return True, logs
+        return False, {}
+
+    @partial(jax.jit, static_argnames="self")
+    def learn_on_batch(self, params: FrozenDict, optimizer_state, batch_samples, batch_probabilities):
+        grad_loss, (losses, unsupported_prob) = jax.grad(self.loss_on_batch, has_aux=True)(
+            params, batch_samples, batch_probabilities
+        )
+        updates, optimizer_state = self.optimizer.update(grad_loss, optimizer_state)
+        params = optax.apply_updates(params, updates)
+
+        return params, optimizer_state, losses, unsupported_prob
+
+    def loss_on_batch(self, params: FrozenDict, samples, batch_probabilities):
+        losses, unsupported_probs = jax.vmap(self.loss, in_axes=(None, None, 0))(params, samples)
+        loss_weights = 1.0 / jnp.sqrt(batch_probabilities + 1e-10)  # sqrt because beta is fixed to 0.5 in PER
+        loss_weights /= jnp.max(loss_weights)
+        return (losses * loss_weights).mean(), (losses, unsupported_probs.mean())
+
+    def loss(self, params: FrozenDict, sample: SubsequenceReplayElement):
+        # computes the loss for a single sample
+        target_support, target_prob = self.compute_target(params, sample)
+        q_logits, latent_predictions, latent_targets = self.network.apply_fn(
+            params, sample.states_stack, sample.actions_stack
+        )
+        q_logits = q_logits[sample.action[0]]
+        projected_target, unsupported_prob = self.project_target_on_support(target_support, target_prob)
+        cross_entropy = optax.softmax_cross_entropy(q_logits, jax.lax.stop_gradient(projected_target))
+        spr_loss = optax.squared_error(latent_predictions, jax.lax.stop_gradient(latent_targets))
+        spr_loss = spr_loss * sample.sample_trajectory_mask
+        return cross_entropy + self.spr_weight * spr_loss, unsupported_prob
+
+    def compute_target(self, params: FrozenDict, sample: SubsequenceReplayElement):
+        # computes the target value for single sample
+        target_support = sample.reward + (1 - sample.is_terminal) * (self.gamma**self.update_horizon) * self.support
+        target_logits = self.apply_fn_inference(params, sample.next_state)
+        target_prob = jax.nn.softmax(target_logits[jnp.argmax(jax.nn.softmax(target_logits) @ self.support)])
+        return target_support, target_prob
+
+    def project_target_on_support(self, target_support: jax.Array, target_prob: jax.Array) -> jax.Array:
+        delta_z = (self.support[-1] - self.support[0]) / (self.n_bins - 1)
+        clipped_support = jnp.clip(target_support, self.support[0], self.support[-1])
+        return (
+            jnp.clip(1 - jnp.abs(clipped_support - self.support[:, None]) / delta_z, 0, 1) @ target_prob,
+            (
+                (
+                    jnp.clip(1 - jnp.abs(clipped_support - self.support[:, None]) / delta_z, 0, 1)[jnp.array([0, -1])]
+                    == 1  # just take probabilities beyond vmin (0) and vmax (-1)
+                )
+                @ target_prob
+            ).sum(),
+        )
+
+    @partial(jax.jit, static_argnames="self")
+    def best_action(self, params: FrozenDict, state: jnp.ndarray, **kwargs):
+        # computes the best action for a single state
+        # We first compute the probabilities by applying the softmax on the last axis (bin axis).
+        # Then, we compute the expectation by multiplying with the bin centers.
+        return jnp.argmax(jax.nn.softmax(self.network.apply_fn(params, state)) @ self.support)
+
+    def get_model(self):
+        return {"params": self.params}
