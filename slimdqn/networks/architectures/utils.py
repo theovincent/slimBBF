bb0f9d0d4cee90866967e462fd8d8fb0c1e5d57a
yogesh1q2w <yogesh120699@gmail.com>
2025-08-16 15:39:57 +0200

    minor fix

--- slimdqn/networks/architectures/utils.py
+++ slimdqn/networks/architectures/utils.py
@@ -93,7 +93,7 @@ def interpolate_weights(old_params, new_params, old_weight, new_weight, keys):
         if k not in keys:
             combined_params[k] = v
 
-    return FrozenDict({"params": combined_params})
+    return {"params": combined_params}
 
 
 def exponential_scheduler(decay_period, initial_value, final_value):
