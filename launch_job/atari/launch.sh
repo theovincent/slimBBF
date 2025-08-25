df250f54322060cbb6a7f09f57c1381a9fa4e6db
yogesh1q2w <yogesh120699@gmail.com>
2025-08-25 17:19:55 +0200

    remove eval flag

--- launch_job/atari/launch.sh
+++ launch_job/atari/launch.sh
@@ -2,7 +2,7 @@ SHARED_ARGS="--features 64 128 128 2048 --replay_buffer_capacity 110_000 --batch
     --min_gamma 0.97 --max_gamma 0.997 --learning_rate 0.0001 \
     --horizon 27_000 --n_sampling_steps 100_000 --update_to_data 2 --n_initial_samples 2_000 \
     --tau 0.005 --epsilon_end 0.0 --epsilon_duration 2_001 --n_bins 51 \
-    --gamma_horizon_decay_steps 10_000 --reset_frequency 20_000 --eval"
+    --gamma_horizon_decay_steps 10_000 --reset_frequency 20_000"
 
 GAME="BattleZone"
 
