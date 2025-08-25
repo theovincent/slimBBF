softmax loss fix

slimbbf/algorithms/architectures/bbf.py
@@ -110,7 +110,7 @@ class BBFNet(nn.Module):
 
         if actions is None:
             # shape (n_actions, n_bins)
-            return nn.softmax(q_logits)
+            return q_logits
         else:
             # shape (n_bins) | (horizon, latent_dimension)
-            return nn.softmax(q_logits)[actions[0]], self.spr_rollout(spatial_latent, actions)
+            return q_logits[actions[0]], self.spr_rollout(spatial_latent, actions)

slimbbf/algorithms/bbf.py
@@ -135,8 +135,8 @@ class BBF:
     ):
         # Only works for a single sample
         target_probs = self.compute_target(params_target, sample, discounted_gamma)
-        q_probs, spr_predictions = self.network.apply(params, sample.states_stack[0], sample.actions_stack[:-1])
-        cross_entropy = importance_weight * optax.softmax_cross_entropy(q_probs, jax.lax.stop_gradient(target_probs))
+        q_logits, spr_predictions = self.network.apply(params, sample.states_stack[0], sample.actions_stack[:-1])
+        cross_entropy = importance_weight * optax.softmax_cross_entropy(q_logits, jax.lax.stop_gradient(target_probs))
 
         # shape (window_size, latent_dimension)
         spr_targets = jax.vmap(partial(self.network.apply, method=self.network.encode_and_project), in_axes=(None, 0))(
@@ -154,7 +154,7 @@ class BBF:
     def compute_target(self, params: FrozenDict, sample: SubsequenceReplayElement, discounted_gamma: float):
         # computes the target value for single sample
         # shape (n_actions, n_bins)
-        target_probs_actions = self.network.apply(params, sample.next_state)
+        target_probs_actions = jax.nn.softmax(self.network.apply(params, sample.next_state))
         target_probs = target_probs_actions[jnp.argmax(target_probs_actions @ self.bins)]
 
         # shape (n_bins)
@@ -205,7 +205,7 @@ class BBF:
     @partial(jax.jit, static_argnames="self")
     def best_action(self, params: FrozenDict, state: jnp.ndarray):
         normalized_state = state.astype(jnp.float32) / 255.0
-        return jnp.argmax(self.network.apply(params, normalized_state) @ self.bins)
+        return jnp.argmax(jax.nn.softmax(self.network.apply(params, normalized_state)) @ self.bins)
 
     def get_logs(self):
         return {"train/td_loss": self.cumulated_td_loss, "train/spr_loss": self.cumulated_spr_loss}

tests/test_bbf.py
@@ -52,7 +52,7 @@ class TestBBF(unittest.TestCase):
         update_horizon = jax.random.randint(self.key, (), minval=1, maxval=10)
         computed_target_probs = self.q.compute_target(self.q.params, sample, gamma**update_horizon)
 
-        target_prob_actions = self.q.network.apply(self.q.params, sample.next_state)
+        target_prob_actions = jax.nn.softmax(self.q.network.apply(self.q.params, sample.next_state))
         target_probs = target_prob_actions[jnp.argmax(jax.nn.softmax(target_prob_actions) @ self.q.bins)]
         target_locations_ = sample.reward + (1 - sample.is_terminal) * (gamma**update_horizon) * self.q.bins
         targets_locations = jnp.clip(target_locations_, self.q.bins[0], self.q.bins[-1])
@@ -98,8 +98,8 @@ class TestBBF(unittest.TestCase):
 
         computed_best_action = self.q.best_action(self.q.params, state)
 
-        q_logits = self.q.network.apply(self.q.params, state / 255.0)
-        best_action = jnp.argmax(q_logits @ self.q.bins)
+        q_probs = jax.nn.softmax(self.q.network.apply(self.q.params, state / 255.0))
+        best_action = jnp.argmax(q_probs @ self.q.bins)
         self.assertEqual(best_action, computed_best_action)
 
 