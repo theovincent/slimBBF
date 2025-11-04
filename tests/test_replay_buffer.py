461c24a684788be66d9e33d9fca1fc84260982b2
yogesh1q2w <yogesh120699@gmail.com>
2025-11-04 12:30:47 +0100

    tests clean

--- tests/test_replay_buffer.py
+++ tests/test_replay_buffer.py
@@ -17,10 +17,8 @@ def make_state(fill_values: list):
 class ReplayBufferTest(unittest.TestCase):
 
     def testAddUpToCapacity(self):
-        capacity = 5
-        add_count = 5
         rb = ReplayBuffer(
-            max_capacity=capacity,
+            max_capacity=5,
             seed=0,
             batch_size=32,
             observation_shape=(84, 84),
@@ -29,17 +27,17 @@ class ReplayBufferTest(unittest.TestCase):
             clipping=None,
         )
 
-        for i in range(add_count):
+        for i in range(5):
             rb.add(np.full((84, 84), i), i, i, False, False)
         # RB starts with [0 0 0# - -] as keys (# = truncation)
-        # After adding obs 0,1,2,3,4 it becomes [2 3 4 0 1]
+        # After adding obs 0,1,2,3,4 it becomes [2 3 4# 0 1]
         expected_keys = [2, 3, 4, 0, 1]
         for idx, key in enumerate(expected_keys):
             np.testing.assert_array_equal(rb.observation_stack[idx], np.full((84, 84), key))
             self.assertEqual(rb.action_stack[idx], key)
             self.assertEqual(rb.reward_stack[idx], key)
             self.assertEqual(rb.is_terminal_stack[idx], False)
-            if idx == capacity - 3:
+            if idx == 2:
                 self.assertEqual(rb.is_truncation_stack[idx], True)
             else:
                 self.assertEqual(rb.is_truncation_stack[idx], False)
@@ -64,72 +62,7 @@ class ReplayBufferTest(unittest.TestCase):
             # Make sure the total reward is reward per step x update_horizon.
             np.testing.assert_array_equal(batch[0].reward, np.ones(32) * 10.0)
 
-    def testGetStack(self):
-        rb = ReplayBuffer(
-            max_capacity=10,
-            seed=0,
-            batch_size=32,
-            observation_shape=(84, 84),
-            observation_dtype=np.uint8,
-            stack_size=4,
-            clipping=None,
-        )
-        rb.add(np.full((84, 84), 1), 1, 1, False, False)
-        rb.add(np.full((84, 84), 2), 2, 2, False, False)
-        rb.add(np.full((84, 84), 3), 3, 3, False, False)
-        # After adding RB is [0 0 0 1 2 3#]
-
-        # ensure that the returned state at index 3 and 4 is correct
-        np.testing.assert_array_equal(rb.check_valid_and_get_sample(3, 1, 1).state, make_state([0, 0, 0, 1]))
-        np.testing.assert_array_equal(rb.check_valid_and_get_sample(4, 1, 1).state, make_state([0, 0, 1, 2]))
-
-        # ensure that the returned action, reward, next_state, is_terminal are correct
-        sample = rb.check_valid_and_get_sample(3, 1, 1)
-        self.assertEqual(sample.action, 1)
-        np.testing.assert_array_equal(sample.next_state, make_state([0, 0, 1, 2]))
-        self.assertEqual(sample.reward, 1)
-        self.assertEqual(sample.is_terminal, False)
-
-        # ensure indices 2,5 are invalid
-        self.assertEqual(rb.check_valid_and_get_sample(2, 1, 1), None)
-        self.assertEqual(rb.check_valid_and_get_sample(5, 1, 1), None)
-
-    def testFillBeyondCapacityWithTerminal(self):
-        rb = ReplayBuffer(
-            max_capacity=10,
-            seed=0,
-            batch_size=2,
-            observation_shape=(84, 84),
-            observation_dtype=np.uint8,
-            stack_size=1,  # testing with stack size 1
-            clipping=None,
-        )
-        num_adds = 50  # The number of transitions to add to the memory.
-
-        # After adding 50 transitions, RB would contain transitions [40* 41 42 43 44* 45 46 47 48* 49#] (* = terminal, # = truncation)
-        for i in range(num_adds):
-            terminal = i % 4 == 0  # Every 4 transitions is terminal.
-            rb.add(np.full((84, 84), i), i, i, terminal, False)
-
-        # index 9 is invalid as we cannot create next_state
-        self.assertEqual(rb.check_valid_and_get_sample(9, 1, 1.0), None)
-
-        # check valid sample at index 2 (s=42, a=42, r=42 + 0.5*43, s'=44, d=False) (for n=2)
-        sample = rb.check_valid_and_get_sample(2, 2, 0.5)  # n=2, gamma=0.5
-        np.testing.assert_array_equal(make_state([42]), sample.state)
-        self.assertEqual(42, sample.action)
-        self.assertEqual(42 + 0.5 * 43, sample.reward)
-        self.assertEqual(False, sample.is_terminal)
-        np.testing.assert_array_equal(make_state([44]), sample.next_state)
-
-        # check valid sample at index 8 (s=48, a=48, r=48, d=True) (s' doesn't matter here)
-        sample = rb.check_valid_and_get_sample(8, 1, 1.0)  # gamma=1.0
-        np.testing.assert_array_equal(make_state([48]), sample.state)
-        self.assertEqual(48, sample.action)
-        self.assertEqual(48, sample.reward)
-        self.assertEqual(True, sample.is_terminal)
-
-    def testUpdateAndSmaple(self):
+    def testUpdateAndSample(self):
         rb = ReplayBuffer(
             max_capacity=10,
             seed=0,
--- tests/test_sum_tree.py
+++ tests/test_sum_tree.py
@@ -1,7 +1,6 @@
 # Inspired by dopamine implementation: https://github.com/google/dopamine/blob/master/tests/dopamine/jax/replay_memory/sum_tree_test.py
 
 import unittest
-import numpy as np
 
 from slimbbf.sample_collection import sum_tree
 
@@ -28,90 +27,24 @@ class SumTreeTest(unittest.TestCase):
             self.assertEqual(self.tree.nodes[leaf_index], 1.0)
 
     def test_set_and_get_values_vectorized(self):
-        self.tree.set(
-            np.array([1, 2], dtype=np.int32),
-            np.array([3.0, 4.0], dtype=np.float32),
-        )
+        self.tree.set([1, 2], [3.0, 4.0])
         self.assertEqual(self.tree.get(1), 3.0)
         self.assertEqual(self.tree.get(2), 4.0)
         self.assertEqual(self.tree.root, 7.0)
 
     def test_set_with_duplicates(self):
-        self.tree.set(
-            np.array([1, 1, 1, 2, 2], dtype=np.int32),
-            np.array([3.0, 3.0, 3.0, 4.0, 4.0], dtype=np.float32),
-        )
+        self.tree.set([1, 1, 1, 2, 2], [3.0, 3.0, 3.0, 4.0, 4.0])
         self.assertEqual(self.tree.get(1), 3.0)
         self.assertEqual(self.tree.get(2), 4.0)
         self.assertEqual(self.tree.root, 7.0)
-
-    def test_capacity_greater_than_requested(self):
-        self.assertGreaterEqual(self.tree.nodes.size, 100)
 
     def test_query_value(self):
         self.tree.set(5, 1.0)
         self.assertEqual(self.tree.query(0.99), 5)
 
-    def test_query_values_vectorized(self):
-        """
-              [2.5]
-           [1.5]  [1.0]
-        [0.5 1.0 0.5 0.5]
-        """
-        tree = sum_tree.SumTree(capacity=4)
-        tree.set(
-            np.array([0, 1, 2, 3], dtype=np.int32),
-            np.array([0.5, 1.0, 0.5, 0.5], dtype=np.float32),
-        )
-        self.assertEqual(tree.root, 2.5)
-        self.assertEqual(tree.depth, 3)
-        self.assertEqual(tree.nodes.size, 7)
-        np.testing.assert_array_equal(
-            tree.query(np.array([1.5, 1.0])),
-            np.array([2, 1], np.int32),
-        )
-
-    def test_update_sum_values(self):
-        """
-              [2.5]
-           [1.5]  [1.0]
-        [0.5 1.0 0.5 0.5]
-        """
-        tree = sum_tree.SumTree(capacity=4)
-        tree.set(
-            np.array([0, 1, 2, 3], dtype=np.int32),
-            np.array([0.5, 1.0, 0.5, 0.5], dtype=np.float32),
-        )
-        tree.set(0, 0.25)
-        self.assertEqual(tree.root, 2.25)
-        self.assertEqual(tree.query(0.249), 0)
-        self.assertEqual(tree.query(0.5), 1)
-        self.assertEqual(tree.query(1.25), 2)
-
-    def test_query_values_vectorized_large_tree(self):
-        """
-                   [8]
-             [4]         [4]
-          [2]   [2]   [2]   [2]
-        [1, 1, 1, 1, 1, 1, 1, 1]
-        """
-        tree = sum_tree.SumTree(capacity=8)
-        tree.set(
-            np.arange(8, dtype=np.int32),
-            np.ones((8,), dtype=np.float32),
-        )
-        self.assertEqual(tree.root, 8.0)
-        self.assertEqual(tree.depth, 4)
-        self.assertEqual(tree.nodes.size, 15)
-        np.testing.assert_array_equal(
-            tree.query(np.arange(8, dtype=np.int32)),
-            np.arange(8, dtype=np.int32),
-        )
-
     def test_max_recorded_priority(self):
-        k = 32
         self.tree.set(0, 0)
         self.assertEqual(self.tree.max_recorded_priority, 1)
-        for i in range(1, k):
+        for i in range(1, 32):
             self.tree.set(i, i)
             self.assertEqual(self.tree.max_recorded_priority, i)
